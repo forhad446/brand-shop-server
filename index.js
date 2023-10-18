fce7b49d7438bf30e24f1907cf924ff31d44204a
forhad446 <forhadkkhan@gmail.com>
2023-10-19 00:18:49 +0600

    insert data from client site to database

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+// forhad445
+// 6aBGHntmhH3iiQ5g
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://forhad445:6aBGHntmhH3iiQ5g@cluster0.k9oitd7.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const productCollection = client.db("productDB").collection("products");
+
+        app.get("/myCart", async (req, res) => {
+            const result = await productCollection.find().toArray();
+            res.send(result);
+        })
+        app.post("/product", async (req, res) => {
+            const product = req.body;
+            console.log(product);
+            const result = await productCollection.insertOne(product);
+            res.send(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send('Mr. Computer server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Server running port: ${port}`);
+})
