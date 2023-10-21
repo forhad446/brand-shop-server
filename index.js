2bb5f4024d7359e951d667dbdbb8b8ee96ac7cdf
forhad446 <forhadkkhan@gmail.com>
2023-10-21 22:48:59 +0600

    env file added on this server to protected our database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -8,11 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// forhad445
-// 6aBGHntmhH3iiQ5g
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://forhad445:6aBGHntmhH3iiQ5g@cluster0.k9oitd7.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k9oitd7.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -47,13 +46,11 @@ async function run() {
         // delete single user
         app.delete("/myCart/:id", async (req, res) => {
             const id = req.params.id;
-            
+
             const query = {
                 _id: new ObjectId(id),
             };
             const result = await productCollection.deleteOne(query);
-            console.log(result);
-            // res.send(result);
         });
 
         app.put("/myCart/:id", async (req, res) => {
@@ -86,7 +83,6 @@ async function run() {
 
         app.post("/product", async (req, res) => {
             const product = req.body;
-            console.log(product);
             const result = await productCollection.insertOne(product);
             res.send(result)
         })
